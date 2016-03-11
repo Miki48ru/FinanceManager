e5d9e96ad9c4fdafa60126211a97c863787b4d20
Miki48RU <miki48ru@gmail.com>
2016-03-11 17:43:08 +0300

    класс исключений по суммам

--- core/src/main/java/com/example/exceptions/AmountException.java
+++ core/src/main/java/com/example/exceptions/AmountException.java
@@ -3,5 +3,26 @@ package com.example.exceptions;
 /**
  * Created by Mike on 11.03.2016.
  */
-public class AmountException {
+public class AmountException extends Exception {
+    public AmountException() {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+
+    public AmountException(String message) {
+        super(message);
+    }
+
+    public AmountException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AmountException(Throwable cause) {
+        super(cause);
+    }
+
+    public AmountException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
 }
