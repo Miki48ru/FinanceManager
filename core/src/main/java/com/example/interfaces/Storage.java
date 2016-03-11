908c23e7f308303286931d25127898226062c0c1
Miki48RU <miki48ru@gmail.com>
2016-03-11 17:43:47 +0300

    добавили конструкторы

--- core/src/main/java/com/example/interfaces/Storage.java
+++ core/src/main/java/com/example/interfaces/Storage.java
@@ -1,5 +1,8 @@
 package com.example.interfaces;
 
+import com.example.exceptions.AmountException;
+import com.example.exceptions.CurrencyException;
+
 import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.List;
@@ -10,30 +13,39 @@ import java.util.Map;
  */
 public interface Storage {
 
+
+
     String getName();
 
     //получение баланса
 
     Map<Currency, BigDecimal> getCurrencyAmounts();
 
-    BigDecimal getAmount(Currency currency);
-    BigDecimal getApproxAmount(Currency currency);
+    BigDecimal getAmount(Currency currency) throws CurrencyException;
+    BigDecimal getApproxAmount(Currency currency) throws CurrencyException;
+
+
+
+
 
     //изменение баланса
 
-    void changeAmount(BigDecimal amount, Currency currency); //изменение баланса по валюте
-    void addAmount(BigDecimal amount, Currency currency); //добавить сумму в валюте
-    void expenseAmount(BigDecimal amount, Currency currency); //отнять сумму в валюте
+
+
+    void changeAmount(BigDecimal amount, Currency currency) throws CurrencyException; //изменение баланса по валюте
+    void addAmount(BigDecimal amount, Currency currency) throws CurrencyException; //добавить сумму в валюте
+    void expenseAmount(BigDecimal amount, Currency currency) throws CurrencyException, AmountException; //отнять сумму в валюте
+
 
 
 
     //работа с валютой
 
-    void addCurrency(Currency currency);
+    void addCurrency(Currency currency) throws CurrencyException;
 
-    void deleteCurrency(Currency currency);
+    void deleteCurrency(Currency currency) throws CurrencyException;
 
-    Currency getCurrency(String code);
+    Currency getCurrency(String code) throws CurrencyException;
 
     List<Currency> getAvailableCurrencies();
 
