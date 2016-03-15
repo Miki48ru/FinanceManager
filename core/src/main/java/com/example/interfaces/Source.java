fce35d09cdcccc4366f38831591c4c643b866e7f
Miki48RU <miki48ru@gmail.com>
2016-03-15 21:21:00 +0300

    Интерфейс для источника

---
+++ core/src/main/java/com/example/interfaces/Source.java
@@ -0,0 +1,13 @@
+package com.example.interfaces;
+
+import com.example.objects.OperationType;
+
+/**
+ * Created by Mike on 15.03.2016.
+ */
+public interface Source {
+
+    String getName();
+
+    OperationType getOperationType();
+}
