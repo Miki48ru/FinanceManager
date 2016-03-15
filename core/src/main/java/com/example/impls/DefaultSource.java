179d9c3b6540fe784cae8b2f26aca91923300c1f
Miki48RU <miki48ru@gmail.com>
2016-03-15 21:24:46 +0300

    новый класс для источников

---
+++ core/src/main/java/com/example/impls/DefaultSource.java
@@ -0,0 +1,71 @@
+package com.example.impls;
+
+import com.example.abstracts.AbstractTreeNode;
+import com.example.interfaces.Source;
+import com.example.interfaces.TreeNode;
+import com.example.objects.OperationType;
+
+import java.util.List;
+
+/**
+ * Created by Mike on 15.03.2016.
+ */
+public class DefaultSource extends AbstractTreeNode implements Source {
+
+    private OperationType operationType;
+
+    public DefaultSource(String name, long id, OperationType operationType) {
+        super(name, id);
+        this.operationType = operationType;
+    }
+
+
+
+    public DefaultSource(long id, List<TreeNode> childs, TreeNode parent, String name) {
+        super(id, childs, parent, name);
+    }
+
+    public DefaultSource(String name, long id) {
+        super(name, id);
+    }
+
+    public DefaultSource(String name) {
+        super(name);
+    }
+
+    public DefaultSource(List<TreeNode> childs) {
+        super(childs);
+    }
+
+    public DefaultSource(long id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public OperationType getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(OperationType operationType) {
+        this.operationType = operationType;
+    }
+
+    @Override
+    public void addChild() {
+
+    }
+
+    @Override
+    public void removeChild() {
+
+    }
+
+    @Override
+    public void addChild(TreeNode child) {
+        if (child instanceof DefaultSource){
+            ((DefaultSource)child).setOperationType(operationType);
+        }
+
+        super.addChild(child);
+    }
+}
