3761b560c9f06cf9243fc02e678912e1535ed5e7
Miki48RU <miki48ru@gmail.com>
2016-03-16 20:08:14 +0300

    абстрактный класс для операций

--- core/src/main/java/com/example/abstracts/AbstractOpiration.java
+++ core/src/main/java/com/example/abstracts/AbstractOpiration.java
@@ -1,7 +1,49 @@
 package com.example.abstracts;
 
+import java.util.Calendar;
+
 /**
  * Created by Mike on 16.03.2016.
  */
-public class AbstractOpiration {
+public abstract class AbstractOpiration {
+    public AbstractOpiration() {
+    }
+
+    public AbstractOpiration(long id, Calendar dateTime, String addInfo) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.addInfo = addInfo;
+    }
+
+    public AbstractOpiration(long id) {
+        this.id = id;
+    }
+
+    private long id;
+    private Calendar dateTime;
+    private String addInfo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Calendar getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Calendar dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getAddInfo() {
+        return addInfo;
+    }
+
+    public void setAddInfo(String addInfo) {
+        this.addInfo = addInfo;
+    }
 }
