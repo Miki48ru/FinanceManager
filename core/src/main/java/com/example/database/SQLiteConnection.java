ff2d7a476b6d84a8e92569ed12f552e24e31ce5f
Miki48RU <miki48ru@gmail.com>
2016-03-19 12:23:42 +0300

    рефактор

--- core/src/main/java/com/example/database/SQLiteConnection.java
+++ core/src/main/java/com/example/database/SQLiteConnection.java
@@ -1,11 +1,33 @@
 package com.example.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by Mike on 19.03.2016.
  */
 public class SQLiteConnection {
 
 
-    private static Conection con;
+    private static Connection con;
 
+
+    public static Connection getConnection() {
+
+        try {
+           Class.forName("org.sqlite.JDBC").newInstance();
+
+           String url = "jdbc:sqlite:c:\\Users\\Mike\\AndroidStudioProjects\\DataBase\\FinanceManager\\mony.db";
+
+           if (con == null) con = DriverManager.getConnection(url);
+
+        return con;
+    } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException ex){
+            Logger.getLogger(SQLiteConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
