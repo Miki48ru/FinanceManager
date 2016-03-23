090cb29db154932125159b81c714e76ada8ccf16
Miki48RU <miki48ru@gmail.com>
2016-03-23 10:49:41 +0300

    интерфейс для паттерна ДАО

--- core/src/main/java/com/example/dao/interfaces/StorageDAO.java
+++ core/src/main/java/com/example/dao/interfaces/StorageDAO.java
@@ -1,7 +1,16 @@
 package com.example.dao.interfaces;
 
+import com.example.interfaces.Storage;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
 /**
  * Created by Mike on 22.03.2016.
  */
-public interface StorageDAO {
+public interface StorageDAO extends CommonDAO<Storage> {
+
+    boolean addCurrency(Storage storage, Currency currency);
+    boolean deleteCurrency (Storage storage, Currency currency);
+    boolean updateAmount(Storage storage, BigDecimal amount);
 }
