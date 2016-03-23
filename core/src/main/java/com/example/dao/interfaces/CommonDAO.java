ef192323089531605b5a5249ddbfa47241f0b4f4
Miki48RU <miki48ru@gmail.com>
2016-03-23 10:49:53 +0300

    реализация паттерна ДАО

--- core/src/main/java/com/example/dao/interfaces/CommonDAO.java
+++ core/src/main/java/com/example/dao/interfaces/CommonDAO.java
@@ -1,7 +1,15 @@
 package com.example.dao.interfaces;
 
+import java.util.List;
+
 /**
  * Created by Mike on 22.03.2016.
  */
-public interface CommonDAO {
+public interface CommonDAO<T> {
+
+    List<T> getAll();
+    T get (long id);
+    boolean update (T storage);
+    boolean delete (T storage);
+
 }
