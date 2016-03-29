e2cca6f6355d3273e37006d6ccf56b49825e05d6
Miki48RU <miki48ru@gmail.com>
2016-03-29 20:59:27 +0300

    code convention

--- core/src/main/java/com/example/abstracts/AbstractOpiration.java
+++ core/src/main/java/com/example/abstracts/AbstractOpiration.java
@@ -6,22 +6,20 @@ import java.util.Calendar;
  * Created by Mike on 16.03.2016.
  */
 public abstract class AbstractOpiration {
+    private long id;
+    private Calendar dateTime;
+    private String addInfo;
+
     public AbstractOpiration() {
     }
-
     public AbstractOpiration(long id, Calendar dateTime, String addInfo) {
         this.id = id;
         this.dateTime = dateTime;
         this.addInfo = addInfo;
     }
-
     public AbstractOpiration(long id) {
         this.id = id;
     }
-
-    private long id;
-    private Calendar dateTime;
-    private String addInfo;
 
     public long getId() {
         return id;
