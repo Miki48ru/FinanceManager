61f6e25d786b584a4b24358725c38a6eb93afc65
Miki48RU <miki48ru@gmail.com>
2016-04-04 13:11:11 +0300

    исходящие операции

---
+++ core/src/main/java/com/example/impls/operations/OutcomeOperation.java
@@ -0,0 +1,58 @@
+package com.example.impls.operations;
+
+import com.example.abstracts.AbstractOperation;
+import com.example.enums.OperationType;
+import com.example.interfaces.Source;
+import com.example.interfaces.Storage;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Currency;
+
+/**
+ * Created by Mike on 16.03.2016.
+ */
+public class OutcomeOperation extends AbstractOperation {
+
+    public OutcomeOperation() {
+        super(OperationType.OUTCOME);
+    }
+
+    private Storage fromStorage; // откуда потратили
+    private Source toSource; // на что потратили
+    private BigDecimal fromAmount; // сумму, которую потратили
+    private Currency fromCurrency; // в какой валюте потратили
+
+
+    public Storage getFromStorage() {
+        return fromStorage;
+    }
+
+    public void setFromStorage(Storage fromStorage) {
+        this.fromStorage = fromStorage;
+    }
+
+    public Source getToSource() {
+        return toSource;
+    }
+
+    public void setToSource(Source toSource) {
+        this.toSource = toSource;
+    }
+
+    public BigDecimal getFromAmount() {
+        return fromAmount;
+    }
+
+    public void setFromAmount(BigDecimal fromAmount) {
+        this.fromAmount = fromAmount;
+    }
+
+    public Currency getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public void setFromCurrency(Currency fromCurrency) {
+        this.fromCurrency = fromCurrency;
+    }
+}
